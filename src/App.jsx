ba832892c43c02e37e58ecf43ffe364fbb71b47c
umercdm <umercdm@gmail.com>
2023-10-01 01:18:33 -0700

    init basic umer cv

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <img src="umer.png" alt="personal pic" />
+
+      <p className="container1">
+        <h1> CURRICULUM VITAI</h1>
+      </p>
+
+      <p className="container2">
+        <h2>Personal Details</h2>
+      </p>
+
+      <h3>Muhammad Umer</h3>
+      <h4>Adress: Walton Road, Lahore Cantt</h4>
+      <h4>Contact: 0321-4617040</h4>
+      <h4>Email: umercdm@gmail.com</h4>
+      <h4>DOB: 12 July 1991</h4>
+      <h4>
+        Linkedin ID:
+        https://www.linkedin.com/in/muhammad-umer-66574b181?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+      </h4>
+      <h4>Githup ID:</h4>
+
+      <p className="container3">
+        <h2>Personal Profile</h2>
+      </p>
+
+      <h4>
+        <ul>Reliable and dependable</ul>
+        <ul>
+          Hard working, possess strong planning, organizing and monitoring
+          abilities
+        </ul>
+        <ul>Motivated, enthusiastic and determined to move forward in life</ul>
+        <ul>Motivated, enthusiastic and determined to move forward in life</ul>
+        <ul>
+          Good communication and interpersonal skills, an efficient time manager
+        </ul>
+      </h4>
+
+      <p className="container4">
+        <h2>Education and Qualification</h2>
+      </p>
+      <h4>
+        {" "}
+        <ul>B.A & B.Ed from University of Punjab</ul>
+      </h4>
+
+      <p className="container5">
+        <h2>Professional Experience</h2>
+      </p>
+      <h4>
+        <ul>
+          Med-IT Solutions as Medical Transcriptionist <br />
+          Worked on Karisma Transcription Software with 7xAustralian Radiology
+          Clinics
+        </ul>
+        <ul>
+          GMT as Medical Transcriptionist <br />
+          Worked on ChartNet 6.00 with King Faisal Specialist Hospital Riyadh
+          and Jeddah Saudi Arabia
+        </ul>
+        <ul>
+          EPCS Switchgear <br />
+          Accounts Manager
+        </ul>
+      </h4>
+
+      <p className="container6">
+        <h2>Computer Skills</h2>
+      </p>
+
+      <h4>
+        <ul>ChartNet 6.00, Karisma Transcription Software, Local ERP</ul>
+        <ul>MS Office (Word, Excel, Power Point, MS Project), AutoCad</ul>
+      </h4>
+      <p className="container7">
+        <h2>Interests</h2>
+      </p>
+
+      <h4>
+        <ul>Cricket</ul>
+        <ul>Traveling</ul>
+      </h4>
+
+      <p className="container8">
+        <h2>Reference</h2>
+      </p>
+
+      <h4>
+        <ul>Would be furnished on demand.</ul>
+      </h4>
+    </div>
+  );
+}
+
+export default App;
